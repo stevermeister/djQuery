a59867966848ef296ff5449d6c2558c573d1b6c4
Stepan Suvorov <stevermeister@gmail.com>
2015-05-02 22:36:35 +0200

    djQuery init

--- body.js
+++ body.js
@@ -1,7 +1,30 @@
-(function(w){
-  var djQuery = function(selector, content){
-    
+(function(w) {
+  var djQuery = function(selector, context) {
+    return new init(selector, context)
   };
+
+  init = function( selector ) {
+
+    if(!selector){
+      return this;
+    }
+
+    var results = document.querySelectorAll(selector);
+    for (var i = 0; i < results.length; i++) {
+      this[i] = results[i];
+    }
+
+    this.length = i;
+  };
+
+  djQuery.prototype = {
+    length: 0,
+    push: [].push,
+    sort: [].sort,
+    splice: [].splice
+  };
+
+  init.prototype = djQuery.prototype;
 
   w.$ = w.djQuery = djQuery;
 })(window);
