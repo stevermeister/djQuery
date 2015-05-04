52cb5318bf34955ab9d2385929351f87a4e89a44
Stepan Suvorov <stevermeister@gmail.com>
2015-05-04 21:42:33 +0200

    $.ready(callback) and $(callback)

--- body.js
+++ body.js
@@ -3,11 +3,18 @@
     return new init(selector, context)
   };
 
-  init = function( selector ) {
+  init = function(selector) {
 
-    if(!selector){
+    if (!selector) {
       return this;
     }
+
+    if(typeof selector === 'function'){
+      djQuery.ready(selector);
+      return this;
+    }
+
+    if(selector)
 
     var results = document.querySelectorAll(selector);
     for (var i = 0; i < results.length; i++) {
@@ -16,6 +23,19 @@
 
     this.length = i;
   };
+
+  //$.ready // DOMContentLoaded
+  var callbacks = [];
+  djQuery.ready = function(callback) {
+    callbacks.push(callback);
+  }
+  function completed() {
+    document.removeEventListener( 'DOMContentLoaded', completed );
+    callbacks.forEach(function(callback) {
+      callback.apply();
+    });
+  }
+  document.addEventListener( 'DOMContentLoaded', completed );
 
   djQuery.prototype = {
     length: 0,
